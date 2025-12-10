78f1b34e4fbbe2397fc3ea01e59f5ff2aed2cf2a
MuhammadHasan440 <hasanwork440@gmail.com>
2025-12-10 13:27:03 -0800

    .

--- app/page.tsx
+++ app/page.tsx
@@ -189,7 +189,7 @@ export default function HomePage() {
             </div>
 
             {/* Responsive typography */}
-       <div className="d-flex justify-content-center align-items-center" style={{ marginLeft: "10px" }}>
+       <div className="d-flex justify-content-center align-items-center" style={{ marginLeft: "3px" }}>
   <img className="w-60 h-60" src="/images/logo.png" alt="" />
 </div>
 
