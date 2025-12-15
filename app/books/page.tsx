372b17e7009a506d443c351511dbf6c5f5d4c166
MuhammadHasan440 <hasanwork440@gmail.com>
2025-12-15 09:11:35 -0800

    add pattern bg

--- app/books/page.tsx
+++ app/books/page.tsx
@@ -336,8 +336,12 @@ export default function BooksPage() {
             background: 'linear-gradient(135deg, #111111 0%, #111111 40%, #A39F3B 100%)',
           }}
         >
+            {/* Background Pattern */}
           <div className="absolute inset-0 opacity-5" style={{
-            backgroundImage: `url("data:image/svg+xml,%3Csvg width='60' height='60' viewBox='0 0 60 60' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%23A39F3B' fill-opacity='0.05'%3E%3Cpath d='M36 34v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zm0-30V0h-2v4h-4v2h4v4h2V6h4V4h-4zM6 34v-4H4v4H0v2h4v4h2v-4h4v-2H6zM6 4V0H4v4H0v2h4v4h2V6h4V4H6z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E")`,
+            backgroundImage: 'url(/images/pattern.jpg)',
+           backgroundPosition: 'center',
+           backgroundRepeat: 'no-repeat',
+           backgroundSize: 'cover',
           }}></div>
           
           <div className="mobile-padding">
